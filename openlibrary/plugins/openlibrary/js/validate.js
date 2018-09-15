67e1cdbf7e39799b8e4ef8b24ed7fb6d9010a2ff
jdlrobson <jdlrobson@gmail.com>
2018-09-15 16:58:45 -0700

    WIP: onsubmit event moved to JavaScript
    (Untested as I need to work out how to get admin rights for the
    http://localhost:8080/libraries/dashboard page)

--- openlibrary/plugins/openlibrary/js/validate.js
+++ openlibrary/plugins/openlibrary/js/validate.js
@@ -13,7 +13,54 @@
  *      </form>
  */
 (function($) {
+    function validate_library_data() {
+        var retval = true;
+        var error_msg = "";
     
+        var re_ip = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+        var re_range = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$$/;
+        var re_cidr = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}$/;
+        var re_comma = /,/;
+        var re_hyphen = /^(\d{1,3}\.\d{1,3}\.\d{1,3})\.(\d{1,3})-(\d{1,3})$/;
+        var re_wild = /^\d{1,3}\.\d{1,3}\.\d{1,3}(-\d{1,3})?\.\*$/;
+        var re_wild2 = /^\d{1,3}\.\d{1,3}\.\*\.\*$/;
+        var re_three = /^(\d{1,3}\.){3}$/;
+
+        var ips = $("#ip_ranges").val().split('\n');
+        $.each(ips, function(index, value) {
+            value = $.trim(value.split('#')[0])
+            if ("" == value) {
+                return true;
+            }
+
+            if (re_comma.test(value)) {
+                retval = false;
+                error_msg = "The IP address " + value + " contains a comma, which is not allowed.";
+                return false;
+            }
+
+            if (!(re_ip.test(value) | re_range.test(value) | re_cidr.test(value) | re_hyphen.test(value) | re_wild.test(value) | re_wild2.test(value) | re_three.test(value))) {
+                retval = false;
+                error_msg = "The IP address/range " + value + " does not match one of the forms that OL accepts.";
+                return false;
+            }
+        });
+
+        if (retval) {
+            var ip_text = $.trim($("#ip_ranges").val());
+            var lending_region = $("#lending_region").val();
+
+            if (("" != ip_text) && ("" != lending_region)) {
+                retval = false;
+                error_msg = "You can either provide IP address ranges or a Lending Region, but not both!";
+            }
+        }
+
+        if (!retval) {
+            alert(error_msg);
+        }
+        return retval;
+    }
 
     // validate publish-date to make sure the date is not in future
     jQuery.validator.addMethod("publish-date", function(value, element) { 
@@ -28,7 +75,6 @@
 
     $.validator.messages.required = "";
     $.validator.messages.email = _("Are you sure that's an email address?");
-    
 
     $.fn.ol_validate = function(options) {
         var defaults = {
@@ -65,5 +111,11 @@
         };
         $(this).validate($.extend(defaults, options));
     };
+
+    // Setup Validation for library edit forms
+    $( function() {
+        $( '.olform[data-ol-type="library"]' )
+            .submit( validate_library_data );
+    } );
 
 })(jQuery);
