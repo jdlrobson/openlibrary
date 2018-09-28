6af737e50c30a4cae2d967ec1d5810d14c9c62c0
jdlrobson <jdlrobson@gmail.com>
2018-09-28 08:35:36 -0700

    Subject page starts with a blank CSS canvas and is v2

--- openlibrary/plugins/worksearch/subjects.py
+++ openlibrary/plugins/worksearch/subjects.py
@@ -43,6 +43,7 @@ class subjects_index(delegate.page):
     path = "/subjects"
 
     def GET(self):
+        delegate.context.setdefault('bodyid', 'subject')
         page = render_template("subjects/index.html")
         page.v2 = True
         return page
@@ -56,6 +57,7 @@ class subjects(delegate.page):
             raise web.redirect(nkey)
 
         subj = get_subject(key, details=True)
+        delegate.context.setdefault('bodyid', 'subject')
         if not subj or subj.work_count == 0:
             web.ctx.status = "404 Not Found"
             page = render_template('subjects/notfound.tmpl', key)
